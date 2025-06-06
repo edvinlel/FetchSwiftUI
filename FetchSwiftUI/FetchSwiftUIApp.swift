8732f32aac11b52887fbc45c226aa7f9d511e23c
Edvin Lellhame <edvinlel@gmail.com>
2025-06-06 12:55:45 -0700

    Initial Commit

---
+++ FetchSwiftUI/FetchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchSwiftUIApp.swift
+//  FetchSwiftUI
+//
+//  Created by Edvin on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
